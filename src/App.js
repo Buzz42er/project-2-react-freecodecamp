7f31468a8f176d81847202465536fce22687e252
Buzz <jure.eres42@gmail.com>
2022-08-07 00:35:47 +0200

    background icon added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './comp/Header';
 import Main from './comp/Main';
 import Footer from './comp/Footer';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header/>
       <Main/>
+      <img className='backgroundLogo' src={logo} alt="logo za pozadinu" />
       <Footer/>
     </div>
   );
