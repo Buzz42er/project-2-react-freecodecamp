51491765b63321e89e53d80d615599cfec36bc00
Buzz <jure.eres42@gmail.com>
2022-09-01 17:41:11 +0200

    1.2.
    setup for 2.0

--- src/comp/Nav.js
+++ src/comp/Nav.js
@@ -3,7 +3,13 @@ import React from "react";
 const Nav = (props) => {
   return (
     <nav className={props.darkMode ? "App-nav dark" : ""}>
-      <p>React Course - Project 1</p>
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={props.toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
     </nav>
   );
 };
